b6dfe5d0f47114fad78c3d36a4a2be9423c4eac1
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-10-12 12:21:19 +0300

    Revert

--- src/main/java/com/ubranians/interview/InterviewMain.java
+++ src/main/java/com/ubranians/interview/InterviewMain.java
@@ -1,6 +1,5 @@
 package com.ubranians.interview;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InterviewMain {
@@ -21,7 +20,7 @@ public class InterviewMain {
      * Change me
      */
     private int[] getFoo() {
-        return new int[magicNumber+1];
+        return new int[magicNumber];
     }
 
 
@@ -33,14 +32,7 @@ public class InterviewMain {
      * Then sort the result in the ascending order and return List.
      */
     List<IntegerHolder> task2(Integer[] array) {
-        List<IntegerHolder> newList = new ArrayList<>();
-        for (Integer i: array) {
-            if (i <= threshold){
-                newList.add(new IntegerHolder(i));
-            }
-        }
-        newList.sort(IntegerHolder::compareTo);
-        return newList;
+        return null;
     }
 
     static class IntegerHolder implements Comparable<IntegerHolder> {
@@ -78,7 +70,7 @@ public class InterviewMain {
      * character from this repeated selection
      * E.g.: "\n\n\n\n\nTEST\n\n\n\t\t\t\t\t\t" -> "\nTEST\n\t"
      */
-    String task3(String input) { // skipped
+    String task3(String input) {
         return null;
     }
 
@@ -93,17 +85,7 @@ public class InterviewMain {
      * ))(()( -> false
      */
     boolean task4(String testString) {
-        int j = 0;
-        for (int i =0; i<testString.length(); i++){
-            if (testString.charAt(i) == openBracket){
-                j++;
-            } else if (testString.charAt(i) == closeBracket){
-                j--;
-            }
-
-            if (j < 0) return false;
-        }
-        return j == 0;
+        return false;
     }
 
     public static void main(String[] args) {
